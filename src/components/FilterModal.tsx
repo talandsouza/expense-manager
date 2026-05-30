0d023d829861af0161fbe7dd3423a4d4c29f9839
Talan Dsouza <65481717+talandsouza@users.noreply.github.com>
2026-05-30 17:03:46 +0530

    Add files via upload

--- src/components/FilterModal.tsx
+++ src/components/FilterModal.tsx
@@ -122,7 +122,7 @@ export default function FilterModal({
   const getPrevMonthYearString = () => getPrevMonthDate().getFullYear().toString();
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center p-4">
+    <div className="fixed inset-0 z-[100] flex items-center justify-center p-4">
       {/* Backdrop */}
       <motion.div
         initial={{ opacity: 0 }}
